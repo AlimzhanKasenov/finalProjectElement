794df39459bb82b542113547316834b976ae28db
AlimzhanKasenov <alim_kasenov@bk.ru>
2023-01-17 19:08:38 +0600

    New

--- app/Http/Controllers/Category/UpdateController.php
+++ app/Http/Controllers/Category/UpdateController.php
@@ -11,9 +11,9 @@ class UpdateController extends Controller
 {
     public function __invoke(UpdateRequest $request, Category $category)
     {
-        $data = $request->validate();
+        $data = $request->validated();
         $category->update($data);
 
-        return view('category.update', compact('category'));
+        return view('category.show', compact('category'));
     }
 }
