1a4003e7a649caead7e69863d92efeae3ff39faf
AlimzhanKasenov <alim_kasenov@bk.ru>
2023-01-17 19:16:19 +0600

    New

--- resources/views/category/show.blade.php
+++ resources/views/category/show.blade.php
@@ -25,9 +25,17 @@
             <div class="row">
                 <div class="col-12">
                     <div class="card">
-                        <div class="card-header">
-                            <a href="{{ route('category.edit', $category->id) }}" class="btn btn-primary">Редактировать</a>
-                        </div>
+                        <div class="card-header d-flex p-3">
+                            <div class="mr-3 ">
+                                <a href="{{ route('category.edit', $category->id) }}" class="btn btn-primary">
+                                    Редактировать</a>
+                            </div>
+                            <form action="{{ route('category.delete', $category->id) }}" method="post">
+                                @csrf
+                                @method('delete')
+                                <input type="submit" class="btn btn-danger" value="Удалить">
+                            </form>
+                            </div>
                         <div class="card-body table-responsive p-0">
                             <table class="table table-hover text-nowrap">
                                 <tbody>
