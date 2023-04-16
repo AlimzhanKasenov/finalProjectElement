95b4eb42965b70be49ad008a8653cc04ca0b7156
AlimzhanKasenov <alim_kasenov@bk.ru>
2023-04-16 08:29:35 +0600

    New

--- app/Http/Controllers/Product/IndexController.php
+++ app/Http/Controllers/Product/IndexController.php
@@ -56,6 +56,37 @@ class IndexController extends Controller
         unset($data['tags'], $data['colors']);
 
         $product = Product::firstOrCreate([ //Надо переделать на артикул
+            'title' => $data['title']
+        ], $data);
+
+        foreach ($tagsIds as $tagsId){
+            ProductTag::firstOrCreate([
+                'product_id' => $product -> id,
+                'tag_id' => $tagsId
+            ]);
+        }
+
+        foreach ($colorsIds as $colorsId){
+            ColorProduct::firstOrCreate([
+                'product_id' => $product -> id,
+                'color_id' => $colorsId
+            ]);
+        }
+        return redirect()->route('products.index');
+    }
+
+    /*
+    public function store(StoreRequest $request)
+    {
+        $data = $request->validated();
+
+        $data['preview_image'] = Storage::disk('public')->put('/images', $data['preview_image']);
+
+        $tagsIds = $data['tags'];
+        $colorsIds = $data['colors'];
+        unset($data['tags'], $data['colors']);
+
+        $product = Product::firstOrCreate([ //Надо переделать на артикул
                 'title' => $data['title']
             ], $data);
 
@@ -74,6 +105,7 @@ class IndexController extends Controller
         }
         return redirect()->route('products.index');
     }
+    */
 
     public function update(UpdateRequest $request, Product $product)
     {
--- resources/views/product/create.blade.php
+++ resources/views/product/create.blade.php
@@ -23,58 +23,59 @@
         <div class="container-fluid">
             <!-- Small boxes (Stat box) -->
             <div class="row">
-                <form action="{{ route('product.store') }}" method="post"> <!--a>enctype="multipart/form-data"</a-->
-                    @csrf
-                    <div class="form-group">
-                        <input type="text" name="title" class="form-control" placeholder="Наименование">
-                    </div>
-                    <div class="form-group">
-                        <input type="text" name="description" class="form-control" placeholder="Описание">
-                    </div>
-                    <div class="form-group">
-                        <textarea name="content" class="form-control" cols="30" rows="10" placeholder="Контент"></textarea>
-                    </div>
-                    <div class="form-group">
-                        <input type="number" name="price" class="form-control" placeholder="Цена">
-                    </div>
-                    <div class="form-group">
-                        <input type="number" name="count" class="form-control" placeholder="Количество на складе">
-                    </div>
-                    <!--div class="form-group">
-                        <div class="input-group">
-                            <div class="custom-file">
-                                <input name="preview_image" type="file" class="custom-file-input" id="exampleInputFile">
-                                <label class="custom-file-label" for="exampleInputFile">Выберите файл</label>
-                            </div>
+                <form action = "{{ route('product.store') }}" method = "post" enctype = "multipart/form-data">
+                @csrf
+                <div class="form-group">
+                    <input type="text" name="title" class="form-control" placeholder="Наименование">
+                </div>
+                <div class="form-group">
+                    <input type="text" name="description" class="form-control" placeholder="Описание">
+                </div>
+                <div class="form-group">
+                    <textarea name="content" class="form-control" cols="30" rows="10" placeholder="Контент"></textarea>
+                </div>
+                <div class="form-group">
+                    <input type="number" name="price" class="form-control" placeholder="Цена">
+                </div>
+                <div class="form-group">
+                    <input type="number" name="count" class="form-control" placeholder="Количество на складе">
+                </div>
+                <div class="form-group">
+                    <div class="input-group">
+                        <div class="custom-file">
+                            <input name="preview_image" type="file" class="custom-file-input" id="exampleInputFile">
+                            <label class="custom-file-label" for="exampleInputFile">Выберите файл</label>
                         </div>
-                    </div-->
-                    <div class="form-group">
-                        <select name="tags" class="tags" multiple="multiple" data-placeholder="Выбирите тэг" style="width: 100%;">
-                            @foreach($tags as $tag)
-                                <option value="{{$tag->id}}">{{$tag->title}}</option>
-                            @endforeach
-                        </select>
                     </div>
-                    <div class="form-group">
-                        <select name="colors" class="colors" multiple="multiple" data-placeholder="Выбирите цвет" style="width: 100%;">
-                            @foreach($colors as $color)
+                </div>
+                <div class="form-group">
+                    <select name="tags[]" class="tags" multiple="multiple" data-placeholder="Выбирите тэг"
+                            style="width: 100%;">
+                        @foreach($tags as $tag)
+                            <option  value="{{$tag->id}}">{{$tag->title}}</option>
+                        @endforeach
+                    </select>
+                </div>
+                <div class="form-group">
+                    <select name="colors[]" class="colors" multiple="multiple" data-placeholder="Выбирите цвет"
+                            style="width: 100%;">
+                        @foreach($colors as $color)
                             <option value="{{$color->id}}">{{$color->title}}</option>
-                            @endforeach
-                        </select>
-                    </div>
-                    <div class="form-group">
-                        <select name="category_id" class="form-control select2" style="width: 100%;">
-                            @foreach($categories as $category)
-                                <option value="{{$category->id}}">{{$category->title}}</option>
-                            @endforeach
-                        </select>
-                    </div>
-                    <div class="form-group">
-                        <input type="submit" class="btn btn-primary" value="Добавить">
-                    </div>
+                        @endforeach
+                    </select>
+                </div>
+                <div class="form-group">
+                    <select name="category_id" class="form-control select2" style="width: 100%;">
+                        @foreach($categories as $category)
+                            <option value="{{$category->id}}">{{$category->title}}</option>
+                        @endforeach
+                    </select>
+                </div>
+                <div class="form-group">
+                    <input type="submit" class="btn btn-primary" value="Добавить">
+                </div>
                 </form>
             </div>
-            <!-- /.row -->
         </div><!-- /.container-fluid -->
     </section>
     <!-- /.content -->
